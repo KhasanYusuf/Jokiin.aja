212c09fdc822916de742f4438a33f9103c37bfca
haryog404 <khariogi@gmail.com>
2024-11-17 20:50:15 +0700

    admin views blade templating + routing

--- resources/views/admin/adminBase.blade.php
+++ resources/views/admin/adminBase.blade.php
@@ -66,5 +66,9 @@
         </div>
 
         @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="{{asset('js/pages/apexcharts.js')}}"></script>
+    <script src="{{asset('js/pages/dashboard.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
 
 </body>
