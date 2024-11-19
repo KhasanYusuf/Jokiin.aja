7cf4e7164f3a9793f583f708b98349f18e375f99
haryog404 <khariogi@gmail.com>
2024-11-19 08:23:24 +0700

    ui penjoki

---
+++ resources/views/penjoki/dashboard-admin.blade.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="{{asset('css/bold.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+
+                        <li class="sidebar-item active">
+                            <a href="dashboard-admin.html" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item  has-sub">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-stack"></i>
+                                <span>Data</span>
+                            </a>
+                            <ul class="submenu ">
+                                <li class="submenu-item ">
+                                    <a href="table-data-admin.html">Admin</a>
+                                </li>
+                                <li class="submenu-item ">
+                                    <a href="table-data-penjoki.html">Penjoki</a>
+                                </li>
+                                <li class="submenu-item ">
+                                    <a href="table-data-pelanggan.html">Pelanggan</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item  ">
+                            <a href="data-penarikan.html" class='sidebar-link'>
+                                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                                <span>Penarikan</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item pt-5 active">
+                            <a href="#" class='sidebar-link' style="background-color: rgb(177, 53, 53);">
+                                <i class="bi bi-box-arrow-left"></i>
+                                <span>Log Out</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            <div class="page-content">
+                <section class="row">
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon purple">
+                                                    <i class="iconly-boldShow"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Pengunjung</h6>
+                                                <h6 class="font-extrabold mb-0">0</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon blue">
+                                                    <i class="iconly-boldProfile"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Penjoki</h6>
+                                                <h6 class="font-extrabold mb-0">0</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon green">
+                                                    <i class="iconly-boldAdd-User"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Pelanggan</h6>
+                                                <h6 class="font-extrabold mb-0">0</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon red">
+                                                    <i class="iconly-boldWallet"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Penghasilan</h6>
+                                                <h6 class="font-extrabold mb-0">0</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Pengunjung</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div id="chart-pengunjung"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Penjoki</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div id="chart-penjoki"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Pelanggan</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div id="chart-pelanggan"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4>Penghasilan</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div id="chart-penghasilan"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
--- resources/views/penjoki/post.blade.php
+++ resources/views/penjoki/post.blade.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="{{asset('css/bold.css')}}">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<style>
-    /* CSS untuk menghilangkan panah */
-    input[type="number"]::-webkit-inner-spin-button,
-    input[type="number"]::-webkit-outer-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    input[type="number"] {
-        -moz-appearance: textfield;
-    }
-</style>
-<body>
+@extends('penjoki/basepenjoki')
+@section('content')
     <div id="app">
-        <div id="sidebar" class="active">
+        <!-- <div id="sidebar" class="active">
             <div class="sidebar-wrapper active">
                 <div class="sidebar-menu">
                     <ul class="menu">
@@ -44,7 +21,7 @@
                                 <span>Dashboard</span>
                             </a>
                         </li>
-                        <li class="sidebar-item  ">
+                        <li class="sidebar-item">
                             <a href="penarikan.html" class='sidebar-link'>
                                 <i class="bi bi-file-earmark-spreadsheet-fill"></i>
                                 <span>Penarikan</span>
@@ -58,74 +35,66 @@
                         </li>
                     </ul>
                 </div>
-                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+                <button class="sidebar-toggler btn x"><i class="bi bi-x"></i></button>
             </div>
-        </div>
+        </div> -->
+
         <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
+
             <div class="page-content">
                 <section class="section">
                     <div class="card">
                         <div class="card-header">
-                            <a href="" class="btn btn-danger" style="margin-bottom: 40px;">Kembali</a>
+                            <a href="#" class="btn btn-danger" style="margin-bottom: 40px;">Kembali</a>
                             <h4 class="card-title">Tambah Produk</h4>
                         </div>
                         <form method="POST" action="/posts" enctype="multipart/form-data">
                             @csrf
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="basicInput">Judul</label>
-                                        <input type="text" class="form-control" id="basicInput" placeholder="" id="title" name="title" required>
-                                    </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="basicInput">Judul</label>
+                                            <input type="text" class="form-control" id="title" name="title" required>
+                                        </div>
 
-                                    <div class="form-group">
-                                        <label for="helpInputTop">Detail</label>
-                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" id="details" name="details" required></textarea>
-                                    </div>
+                                        <div class="form-group">
+                                            <label for="helpInputTop">Detail</label>
+                                            <textarea class="form-control" id="details" name="details" rows="3" required></textarea>
+                                        </div>
 
-                                    <div class="form-group">
-                                        <label for="helperText">Durasi Pengerjaan</label>
-                                        <input type="number" id="helperText" class="form-control" placeholder="" id="duration" name="duration" required>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="disabledInput">Harga</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)" id="price" name="price" required>
+                                        <div class="form-group">
+                                            <label for="helperText">Durasi Pengerjaan</label>
+                                            <input type="number" class="form-control" id="duration" name="duration" required>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="readonlyInput">Gambar</label>
-                                        <fieldset>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="disabledInput">Harga</label>
                                             <div class="input-group mb-3">
+                                                <span class="input-group-text">Rp</span>
+                                                <input type="text" class="form-control" id="price" name="price" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="readonlyInput">Gambar</label>
+                                            <fieldset>
                                                 <div class="input-group mb-3">
                                                     <label class="input-group-text" for="inputGroupFile01"><i class="bi bi-upload"></i></label>
                                                     <input type="file" class="form-control" id="inputGroupFile01" name="images[]" multiple>
                                                 </div>
-                                            </div>
-                                        </fieldset>
+                                            </fieldset>
+                                        </div>
+                                        <button type="submit" class="btn btn-success">Submit</button>
                                     </div>
-                                    <button type="submit" class="btn btn-success">Submit</button>
-                        </form>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </section>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
-    <script src="assets/js/pages/dashboard.js"></script>
-    <script src="assets/js/main.js"></script>
-</body>
-</html>
+    @endsection
