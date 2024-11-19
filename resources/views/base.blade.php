c67cd4574eb14b266c4a1d6ee890f7ae3ffe66bd
haryog404 <khariogi@gmail.com>
2024-11-19 09:18:49 +0700

    profile ui fix

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -68,7 +68,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="./"><img src="img/logo.png"></a></h1>
+                        <h1><a href="{{url('/')}}"><img src="{{asset('img/logo.png')}}"></a></h1>
                     </div>
                 </div>
 
